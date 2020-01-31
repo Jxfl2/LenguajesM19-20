2814277a9d4ea78e354656619c7ad21fb9e8f42f
aalv05 <aalvarezv05@larioja.edu.es>
2020-01-31 21:54:26 +0100

    javascript

---
+++ Proyecto_Aaron/scripts.js
@@ -0,0 +1,9 @@
+
+function mostrarMenu () {
+    if(document.getElementById("menuDesplegable").display == "none"){
+        document.getElementById("menuDesplegable").display = "block";
+    } else {
+        document.getElementById("menuDesplegable").display = "none";
+    }
+} 
+
