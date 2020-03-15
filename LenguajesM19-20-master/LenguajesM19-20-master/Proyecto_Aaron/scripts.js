af7236b540908012ee199abbe38302a09bebc0de
Jxfl2 <58531117+Jxfl2@users.noreply.github.com>
2020-03-15 19:48:37 +0100

    Cambiar el index

---
+++ LenguajesM19-20-master/LenguajesM19-20-master/Proyecto_Aaron/scripts.js
@@ -0,0 +1,8 @@
+function mostrarMenu() {
+    if(document.getElementById("menuDesplegable").style.display == "none"){
+        document.getElementById("menuDesplegable").style.display = "block";
+    } else {
+        document.getElementById("menuDesplegable").style.display = "none";
+    }
+} 
+
