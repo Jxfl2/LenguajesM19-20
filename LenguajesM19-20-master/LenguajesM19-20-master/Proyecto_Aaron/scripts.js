707e90b9aaf7a729a6880dfeb4287aa2cc238a3e
Jxfl <58531117+Jxfl2@users.noreply.github.com>
2020-03-25 17:43:07 +0100

    borrar carpeta duplicada
    borrar carpeta duplicada

--- LenguajesM19-20-master/LenguajesM19-20-master/Proyecto_Aaron/scripts.js
+++
@@ -1,8 +0,0 @@
-function mostrarMenu() {
-    if(document.getElementById("menuDesplegable").style.display == "none"){
-        document.getElementById("menuDesplegable").style.display = "block";
-    } else {
-        document.getElementById("menuDesplegable").style.display = "none";
-    }
-} 
-
